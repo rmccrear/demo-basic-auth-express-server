8e18fa6f96ff1c7d964673ad150c114aeee20718
Robert Mccreary <fooyinfighter@gmail.com>
2022-09-26 18:24:15 -0700

    move app.js to src directory

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-const app = require("../app");
+const app = require("../src/app");
 const sequelize = require("../db");
 
 const supertest = require("supertest");
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const base64 = require("base-64");
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("./db");
+const sequelize = require("../db");
 
 // Prepare the express app
 const app = express();
